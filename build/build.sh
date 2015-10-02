e146dfd470b0aa1505c8e5f0540e16e2d419ce90
Helio Perroni Filho <xperroni@gmail.com>
2015-10-02 11:22:05 +0900

    Removed unneeded files
    Signed-off-by: Helio Perroni Filho <xperroni@gmail.com>

--- build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MODE=$(echo ${PWD##*/} | sed 's/[^ ]\+/\L\u&/g')
-
-cmake -DCMAKE_BUILD_TYPE=$MODE ../.. #-DBUILD_SHARED_LIBS:BOOL=ON
-
-make -j2
--- build/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Remove all files and folder, save from shell script files
-rm -fR $(ls | grep -v '\.sh')
